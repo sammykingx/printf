9ff76d99dd8b3a61a338279eeae674ceab1ae017
sammykingx <hellosammy@sammykingx.com.ng>
2022-07-10 10:40:50 -0700

    tried doing the printf again

--- _printf.c
+++ _printf.c
@@ -1,50 +1,49 @@
 #include "main.h"
 /**
- * _printf - prints and input into the standard output
- * @format: the format string
- * Return: number of bytes printed
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-
-int _printf(const char *format, ...)
-
+int _printf(char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
+int written = 0, (*structype)(char *, va_list);
+char q[3];
+va_list pa;
 
-	params_t params = PARAMS_INIT;
-
-	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
-	{
-		init_params(&params, ap);
-		if (*p != '%')/*checking for the % specifier*/
-		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag character */
-		{
-			p++; /* next character */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-					params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
-	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+if (format == NULL)
+return (-1);
+q[2] = '\0';
+va_start(pa, format);
+_putchar(-1);
+while (format[0])
+{
+if (format[0] == '%')
+{
+structype = driver(format);
+if (structype)
+{
+q[0] = '%';
+q[1] = format[1];
+written += structype(q, pa);
+}
+else if (format[1] != '\0')
+{
+written += _putchar('%');
+written += _putchar(format[1]);
+}
+else
+{
+written += _putchar('%');
+break;
+}
+format += 2;
+}
+else
+{
+written += _putchar(format[0]);
+format++;
+}
+}
+_putchar(-2);
+return (written);
 }
